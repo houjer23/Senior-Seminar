792e30a41a2346dcf3025e8cf8e28a28090a2a6f
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-03-07 11:07:47 -0500

    Create Activity.java

---
+++ Activity.java
@@ -0,0 +1,17 @@
+/**
+* This class gives the description of one activity
+*
+* @author Jerry Hou
+* @since 1.0
+*/
+
+public class Activity {
+	String name;
+	String presenter;
+	
+	// Constructor for the Activity
+	public Activity(String name, String presenter) {
+		this.name = name;
+		this.presenter = presenter;
+	} // end of the Constructor
+} // end of the class
