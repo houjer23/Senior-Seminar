41655e8c80a895ec2ff43c271fc53abde2c44c10
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-03-17 08:14:04 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,9 +6,9 @@
 */
 
 public class Student {
-	String email;
-	String name;
-	int[] choice;
+	private String email;
+	private String name;
+	private int[] choice;
 	
 	// constructor for student, storing the file
 	public Student(String email, String name, int[] choice) {
@@ -21,4 +21,9 @@ public class Student {
 	public String toString() {
 		return email + " " + name;
 	} // end of the toString method
+
+	// getter method for student choice
+	public int[] get_choice() {
+		return choice;
+	} // end of the getter method
 } // end of the class
