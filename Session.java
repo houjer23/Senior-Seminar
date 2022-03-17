a8c74dacaed54616a45599e7cce6050e4f5cecdc
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-03-17 08:14:38 -0400

    Create Session.java

---
+++ Session.java
@@ -0,0 +1,31 @@
+/**
+* This construct the Session and help sort the Session based on user choice
+*
+* @author Jerry Hou
+* @since 1.0
+*/
+public class Session implements Comparable<Session>{
+	private int id;
+	private int popularity;
+
+	// Constructor
+	public Session(int id, int popularity) {
+		this.id = id;
+		this.popularity = popularity;
+	}
+	
+	// compareTo used for sort
+	public int compareTo(Session s2) {
+		return s2.popularity - this.popularity;
+	} // end of compareTo method
+
+	// setter method for popularity
+	public void increase_popularity(int amount) {
+		popularity += amount;
+	} // end of the setter method
+	
+	// rewrite toString method
+	public String toString() {
+		return "Session Id: " + id + " Popularity: " + popularity;
+	} // end of the toString method
+}
