c00cb8e0912a3cd4533114c32246f4ecc4f07758
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-03-17 19:09:29 -0400

    Update Tester.java
    Finish the game

--- Tester.java
+++ Tester.java
@@ -29,10 +29,8 @@ public class Tester {
 	public static List<List<List<Integer>>> students_in_session = new ArrayList<>();
 	// the code runs here
 	public static void main(String[] args) throws IOException {
-		// Store all inputs to the arraylist
-        Scanner scanner = new Scanner(new File("SrSeminar_Arranged.csv"));
-        scanner.useDelimiter(",");
-        ArrayList<String> input = new ArrayList<>();
+		// Store all inputs to a deque
+        Deque<String> input = new ArrayDeque<>();
         BufferedReader csvReader = new BufferedReader(new FileReader("SrSeminar_Arranged.csv"));
         String row;
 		while ((row = csvReader.readLine()) != null) {
@@ -42,8 +40,7 @@ public class Tester {
 			}
 		}
 		csvReader.close();
-        System.out.println(input);
-        scanner.close();
+        // call this method to arrange the input
         store_data(input);
         
         // prints students and activies
@@ -55,12 +52,12 @@ public class Tester {
 		}
 		System.out.println("\n\n\n");
 		
-		// rank popularity and print out popularity
+		// rank popularity of the activity and print out popularity
 		sort_popular_session();
 		for (int i = 0; i < sessions.length; i ++) {
 			System.out.println(sessions[i]);
 		}
-		System.out.println("\n\n");
+		System.out.println("\n\n\n");
 		
 		// contruct the metting id
 		construct_time_spot();
@@ -69,29 +66,34 @@ public class Tester {
 		for (int i = 0; i < 5; i ++) {
 			System.out.println(session_placement.get(i));
 		}
+		System.out.println("\n\n\n");
 		
+		// puts students into an ideal spot
 		add_students();
-		
+		for (int i = 0; i < students_in_session.size(); i ++) {
+			System.out.println(students_in_session.get(i));
+		}
     } // end of public static void main
     
     // pass the arraylist with all inputs to the store data method and construct Student and Activity
-    public static void store_data(ArrayList<String> input) {
+    public static void store_data(Deque<String> input) {
 		// construct Student
         for (int i = 0; i < 69; i ++) {
-			String email = input.remove(0);
-			String name = input.remove(0) + " " + input.remove(0);
+			String email = input.pop();
+			String name = input.pop() + " " + input.pop();
 			int[] choice = new int[5];
 			for (int j = 0; j < 5; j ++) {
-				choice[j] = Integer.parseInt(input.remove(0));
+				//System.out.println("\n\n\n" + a + "\n\n\n");
+				choice[j] = Integer.parseInt(input.pop());
 			}
 			Student s = new Student(email, name, choice);
 			students[i] = s;
 		}
 		// construct Activity
 		while (input.size() > 0) {
-			String name = input.remove(0);
-			int id = Integer.parseInt(input.remove(0));
-			String presenter = input.remove(0);
+			String name = input.pop();
+			int id = Integer.parseInt(input.pop());
+			String presenter = input.pop();
 			activities[id - 1] = new Activity(name, presenter);
 		}
 	} // end of store data method
@@ -124,13 +126,13 @@ public class Tester {
 				} else if (j == 1) {
 					index_to_loop = IntStream.of(4, 0, 1, 2, 3).toArray();
 				} else if (j == 2) {
-					index_to_loop = IntStream.of(5, 6, 7, 8, 9).toArray();
+					index_to_loop = IntStream.of(5, 7, 8, 9, 6).toArray();
 				} else if (j == 3) {
-					index_to_loop = IntStream.of(9, 5, 6, 7, 8).toArray();
+					index_to_loop = IntStream.of(9, 5, 6, 8, 7).toArray();
 				} else if (j == 4) {
 					index_to_loop = IntStream.of(10, 11, 12, 13, 14).toArray();
 				} else {
-					index_to_loop = IntStream.of(15, 16, 17, 10, 11).toArray();
+					index_to_loop = IntStream.of(11, 15, 16, 17, 10).toArray();
 				}
 				session_placement.get(i).add(sessions[index_to_loop[i]].get_id());
 			}
@@ -145,9 +147,6 @@ public class Tester {
 			for (int j = 0; j < 6; j ++) {
 				students_in_session.get(i).add(new ArrayList<>());
 			}
-		}
-		for (int i = 0; i < students_in_session.size(); i ++) {
-			System.out.println(students_in_session.get(i));
 		}
 		// unpaired staudent: [student id, time spot]
 		Deque<List<Integer>> unpaired_students = new ArrayDeque<>();
@@ -165,7 +164,7 @@ public class Tester {
 						break;
 					}
 					for (int l = 0; l < 6; l ++) { // loop through each session
-						if (session_placement.get(j).get(l) == choice.get(k) && students_in_session.get(j).get(l).size() <= 15) {
+						if (session_placement.get(j).get(l) == choice.get(k) && students_in_session.get(j).get(l).size() < 15) {
 							students_in_session.get(j).get(l).add(i);
 							choice.remove(k);
 							//System.out.println(choice + " " + i);
@@ -179,22 +178,19 @@ public class Tester {
 					cur_student_unpaired.add(i);
 					cur_student_unpaired.add(j);
 					unpaired_students.add(cur_student_unpaired);
-					//System.out.println(choice + " " + i);
-				}
-				for (int k = 0; k < students_in_session.size(); k ++) {
-					//System.out.println(students_in_session.get(k));
 				}
 			}
-			System.out.println(choice + " " + i);
 		}
-		System.out.println(unpaired_students);
-		for (int i = 0; i < students_in_session.size(); i ++) {
-			System.out.println(students_in_session.get(i));
+		// Here, it puts the students who are unpaired at a specific time to one spot
+		// unpaired staudent: [student id, time spot]
+		while (unpaired_students.size() > 0) {
+			List<Integer> cur_student = unpaired_students.pop();
+			//System.out.println(cur_student);
+			for (int i = 5; i >= 0; i --) {
+				if (students_in_session.get(cur_student.get(1)).get(i).size() < 15) {
+					students_in_session.get(cur_student.get(1)).get(i).add(cur_student.get(0));
+				}
+			}
 		}
-		
-		/*while (unpaired_students.size() > 0) {
-			List<Integer> cur_student = unpaired_student.push();
-			
-		}*/
-	}
+	} // end of the putting student into idea spot method
 } // end of Tester class
